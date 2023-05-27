a1d2327272cba8d15ff59881d9c46ffc6ae270f8
KatarzynaLepek <kasia.gliszczynska@gmail.com>
2023-05-27 14:31:51 +0200

    Created app functions

--- app.py
+++ app.py
@@ -1,91 +1,73 @@
-plec = input("Wybierz płeć (m/k):  ")
-# Sprawdzenie wyboru płci
-if plec == "m":
-    print("Wybrałeś mężczyznę.")
-    while True:
-        region = input("Wybierz swoj region: EUR/USA  ")
-        if region == "EUR":
-            print("W Europie picie alkoholu dozwolone jest od 18 r.z.")
-            wiek = input("Podaj wiek użytkownika jako liczbe calkowitą:  ")
-            if wiek.isdigit() == False:
-                exit("Wiek musi być liczbą albo podana liczba nie jest calkowita")
-            wiek = int(wiek)
-            if wiek >= 18 and wiek < 40:
-                print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-                break
-            elif wiek >= 40 and wiek <= 119:
-                print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-                print("Uważaj w Twoim wieku nie przasadzaj ze spożyciem")
-                break
-            elif wiek >= 120:
-                exit("Wpisano zbyt wysoki wiek. Zamykam aplikacje")
-            else:
-                exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
-        elif region == "USA":
-            print("W USA picie alkoholu dozwolone jest od 21 r.z.")
-            wiek = input("Podaj wiek użytkownika jako liczbe calkowitą:  ")
-            if wiek.isdigit() == False:
-                exit("Wiek musi być liczbą albo podana liczba nie jest calkowita")
-            wiek = int(wiek)
-            if wiek >= 21 and wiek < 40:
-                print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-                break
-            elif wiek >= 40 and wiek <= 119:
-                print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-                print("Uważaj w Twoim wieku nie przasadzaj ze spożyciem")
-                break
-            elif wiek >= 120:
-                exit("Wpisano zbyt wysoki wiek. Zamykam aplikacje")
-            else:
-                exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
-        else:
-            print("Wybrales nieprawidlowy region. Sprobuj jeszcze raz.  ")
+#funkcja sprawdzajaca plec
+def plec():
+    plec = input("Wybierz plec: M/K   ").upper()
+    if (plec == "K"):
+        return "K"
+    elif (plec == "M"):
+        return "M"
+    else:
+        exit ("Niepoprawny wybor plci - zamykam program")
 
-elif plec == "k":
-    print("Wybrałaś kobietę.")
-    while True:
-        region = input("Wybierz swoj region: EUR/USA  ")
-        if region == "EUR":
-           print("W Europie picie alkoholu dozwolone jest od 18 r.z.")
-           wiek = input("Podaj wiek użytkownika jako liczbe calkowitą:  ")
-           if wiek.isdigit() == False:
-               exit("Wiek musi być liczbą albo podana liczba nie jest calkowita")
-           wiek = int(wiek)
-           if wiek >= 30 and wiek <120:
-               print("Zapraszamy na darmowy Aperol")
-               break
-           elif wiek >= 18 and wiek < 40:
-               print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-               break
-           elif wiek >= 40 and wiek <= 119:
-               print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-               print("Uważaj w Twoim wieku nie przasadzaj ze spożyciem")
-               break
-           elif wiek >= 120:
-               exit("Wpisano zbyt wysoki wiek. Zamykam aplikacje")
-           else:
-               exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
-        elif region == "USA":
-           print("W USA picie alkoholu dozwolone jest od 21 r.z.")
-           wiek = input("Podaj wiek użytkownika jako liczbe calkowitą:  ")
-           if wiek.isdigit() == False:
-               exit("Wiek musi być liczbą albo podana liczba nie jest calkowita")
-           wiek = int(wiek)
-           if wiek >= 30 and wiek <120:
-               print("Zapraszamy na darmowy Aperol")
-               break
-           elif wiek >= 21 and wiek < 40:
-               print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-               break
-           elif wiek >= 40 and wiek <= 119:
-               print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-               print("Uważaj w Twoim wieku nie przasadzaj ze spożyciem")
-               break
-           elif wiek >= 120:
-               exit("Wpisano zbyt wysoki wiek. Zamykam aplikacje")
-           else:
-               exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
-        else:
-            print("Wybrales nieprawidlowy region. Sprobuj jeszcze raz.")
-else:
-    exit("Niepoprawny wybór płci.")
+#funkcja sprawdzajaca wiek
+def wiek():
+    wiek = input("Podaj wiek użytkownika jako liczbe calkowitą:  ")
+    if wiek.isdigit() == False:
+        exit("Wiek musi być liczbą albo podana liczba nie jest calkowita")
+    wiek = int(wiek)
+    return wiek
+
+#funkcja sprawdzajaca region
+def region():
+    region = input ("Program obsluguje tylko Europe i USA, wybierz swoj region: EUR/USA   ")
+    if (region == "EUR"):
+        print ("W Europie picie alkoholu dozwolone jest od 18 r.z.")
+        eur()
+    elif (region == "USA"):
+        print("W USA picie alkoholu dozwolone jest od 21 r.z.")
+        usa()
+    else:
+        exit ("Nie wpisales wlasciwego regionu. Zamykam program")
+
+#funkcja obslugujaca Europe
+def eur():
+    age = wiek()
+    if age >= 18 and age < 40:
+        print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
+
+    elif age >= 40 and age <= 119:
+        print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
+        print("Uważaj w Twoim wieku nie przasadzaj ze spożyciem")
+
+    elif age >= 120:
+        exit("Wpisano zbyt wysoki wiek. Zamykam aplikacje")
+    else:
+        exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
+
+#funkcja obslugujaca USA
+def usa():
+    age = wiek()
+    if age >= 21 and age < 40:
+        print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
+
+    elif age >= 40 and age <= 119:
+        print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
+        print("Uważaj w Twoim wieku nie przasadzaj ze spożyciem")
+
+    elif age >= 120:
+        exit("Wpisano zbyt wysoki wiek. Zamykam aplikacje")
+    else:
+        exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
+
+
+
+#funkcja oferujaca darmowy aperol dla pan
+
+###TODO: ogarnac, zeby dwa razy sie nie pytalo o wiek
+def aperol_kobiety():
+    if plec() == "K" and wiek() >= 30:
+        print("Zapraszamy na darmowy Aperol")
+
+####START PROGRAMU####
+print("Witamy w naszym sklepie z alkoholem")
+region()
+aperol_kobiety()
